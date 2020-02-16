987bc4f87ec4cd41e9915c67e6b23048530a81c5
minbaby.zhang <minbaby.zhang@gmail.com>
2020-02-16 13:56:32 +0800

    判断回文

---
+++ 9.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import (
+	"strconv"
+)
+
+func isPalindrome(x int) bool {
+	str := strconv.Itoa(x)
+	ret := true
+
+	l := len(str)
+	for i := 0; i < l/2; i++ {
+		if str[i] != str[l-i-1] {
+			ret = false
+			break
+		}
+	}
+
+	return ret
+}
