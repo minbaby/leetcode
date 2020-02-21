088de98634250db2f53f39e7697062facc906192
minbaby.zhang <zhangshaomin_1990@126.com>
2020-02-21 17:54:08 +0800

    8. 字符串转换整数 (atoi)
    这个写的真实丑陋，不过第一遍，是为了完成。

---
+++ 8_test.go
@@ -0,0 +1,82 @@
+package leetcode
+
+import "testing"
+
+func Test_myAtoi(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"42",
+			args{"42"},
+			42,
+		},
+		{
+			"-42",
+			args{"-42"},
+			-42,
+		},
+		{
+			"4193 with words",
+			args{"4193 with words"},
+			4193,
+		},
+		{
+			"words and 987",
+			args{"words and 987"},
+			0,
+		},
+		{
+			"-91283472332",
+			args{"-91283472332"},
+			-2147483648,
+		},
+		{
+			" ",
+			args{" "},
+			0,
+		},
+		{
+			"+1",
+			args{"+1"},
+			1,
+		},
+		{
+			"2147483646",
+			args{"2147483646"},
+			2147483646,
+		},
+		{
+			"9223372036854775808",
+			args{"9223372036854775808"},
+			2147483647,
+		},
+		{
+			"  0000000000012345678",
+			args{"  0000000000012345678"},
+			12345678,
+		},
+		{
+			"010",
+			args{"010"},
+			10,
+		},
+		{
+			"-2147483647",
+			args{"-2147483647"},
+			-2147483647,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := myAtoi(tt.args.str); got != tt.want {
+				t.Errorf("myAtoi() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
