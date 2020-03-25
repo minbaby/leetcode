cbc46348bff7b2b5651afec30e7f17987afe4391
minbaby.zhang <zhangshaomin_1990@126.com>
2020-03-25 23:49:19 +0800

    close #3 206. 反转链表 初始化
    核心思路是直接操作当前节点，会导致下一个节点出问题，所以需要临时变量来保存

--- 206.go
+++ 206.go
@@ -1,22 +1,14 @@
 package leetcode
 
 func reverseList(head *ListNode) *ListNode {
-
 	var tail *ListNode
-
 	point := head
 	var tmp *ListNode
-	for point != nil && point.Next != nil {
-
-		if tail == nil {
-			tail = point
-		} else {
-			tmp = tail
-			tail = point
-			tail.Next = tmp
-		}
-
-		point = point.Next
+	for point != nil {
+		tmp = point.Next // 直接操作当前节点的化会导致无法找到下一个节点，所以这个地方做个保存
+		point.Next = tail
+		tail = point
+		point = tmp
 	}
 
 	return tail
