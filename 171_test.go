c6c7e2ee465b5d40d466761910bf7582d85dd254
minbaby.zhang <minbaby.zhang@gmail.com>
2020-03-27 18:13:29 +0800

    close #5 171. Excel表列序号
    1. 我的思路是直接拿map 把所有的映射放上去，这个就可以完成字母到数字的转换
    
    2. 看了题解，有一个新的思路，就是把 A 看做初始值 1 ，那么其他字母和 A 的差 加上初始值 1 就是这个字母对应的数字。

---
+++ 171_test.go
@@ -0,0 +1,42 @@
+package leetcode
+
+import "testing"
+
+func Test_titleToNumber(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"A",
+			args{"A"},
+			1,
+		},
+		{
+			"AB",
+			args{"AB"},
+			28,
+		},
+		{
+			"ZY",
+			args{"ZY"},
+			701,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := titleToNumber(tt.args.s); got != tt.want {
+				t.Errorf("titleToNumber() = %v, want %v", got, tt.want)
+			}
+		})
+		t.Run(tt.name+"#2", func(t *testing.T) {
+			if got := titleToNumber2(tt.args.s); got != tt.want {
+				t.Errorf("titleToNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
