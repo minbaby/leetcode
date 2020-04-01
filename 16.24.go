2ecc5f6877d28c36689fe44cca5e1c1cb947d3fc
minbaby.zhang <zhangshaomin_1990@126.com>
2020-04-01 23:12:43 +0800

    close #8 2020年04月01日 面试题 16.24. 数对和
    日常思路肯定会超时的，毕竟有10W元素。
    
    这里的思路是用map[int]int 来统计个数字出现的次数

---
+++ 16.24.go
@@ -0,0 +1,65 @@
+package leetcode
+
+import "math"
+
+func pairSums(nums []int, target int) [][]int {
+	ret := [][]int{}
+
+	m := map[int]int{}
+
+	var v int
+	var vv int
+	for i := 0; i < len(nums); i++ {
+		v = nums[i]
+		vv = m[target-v]
+		if vv >= 1 {
+			m[target-v] --
+			ret = append(ret, []int{v, target - v})
+		} else {
+			m[v] ++
+		}
+	}
+
+	return ret
+}
+
+// 低效方式
+func pairSums0(nums []int, target int) [][]int {
+	ret := [][]int{}
+
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		t := target - nums[i]
+		if nums[i] == math.MinInt32 {
+			continue
+		}
+		for j := i + 1; j < l; j++ {
+			if nums[j] == math.MinInt32 {
+				continue
+			}
+			if t == nums[j] {
+				a, b := nums[i], nums[j]
+				if a > b {
+					a, b = b, a
+				}
+				ret = append(ret, []int{a, b})
+				nums[j] = math.MinInt32
+				break
+			}
+		}
+
+		nums[i] = math.MinInt32
+	}
+
+	l = len(ret)
+
+	for i := l; i >= 0; i-- {
+		for j := i; j < l-1; j++ {
+			if ret[j][0] > ret[j+1][0] {
+				ret[j], ret[j+1] = ret[j+1], ret[j]
+			}
+		}
+	}
+
+	return ret
+}
