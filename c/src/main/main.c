37a64ae723fc7ef194f232574dfa5d9333c7bcd2
霸气千秋 <zhangshaomin.1990@126.com>
2020-11-30 15:09:17 +0800

    21 合并两个有序链表

--- c/src/main/main.c
+++ c/src/main/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include "20-valid-parentheses/20.h"
+#include "21-merge-two-sorted-lists/21.h"
+#include "../utils.h"
 
 int main(int args, char **arv) {
-    char *input = "((";
-    bool ret = isValid(input);
+    int d[] = {-9, 3};
+    struct ListNode *l1 = utils_list_create(d, 2);
 
-    printf("<><>%d<><>", ret);
+    int d1[] = {5, 7};
+    struct ListNode *l2 = utils_list_create(d1, 2);
+
+    struct ListNode *l3 = mergeTwoLists(l1, l2);
+
+
+    UPrintList(l3);
 }
